6914122f84fc5167054b70c630f02b27d0345c1b
Casen Luck <casenluck@gmail.com>
2021-12-26 17:07:30 -0500

    added more files

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,20 +1,35 @@
 const router = require('express').Router();
 const { User } = require('../models');
+const withAuth = require('../utils/auth');
 
+// TODO: Add a comment describing the functionality of the withAuth middleware
+router.get('/', withAuth, async (req, res) => {
+  try {
+    const userData = await User.findAll({
+      attributes: { exclude: ['password'] },
+      order: [['name', 'ASC']],
+    });
 
-router.get('/', async (req, res) => {
-  // TODO: Render template with Sequelize data
-    
-      const userData = await User.findAll({
-        order: [['name', 'ASC']],
-        attributes: {exclude: ['password']}
-      });
+    const users = userData.map((project) => project.get({ plain: true }));
 
-      const users = userData.map(user => user.get({plain: true}));
+    res.render('homepage', {
+      us,
+      // TODO: Add a comment describing the functionality of this property
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-      res.render('homepage', {users});
- });
+router.get('/login', (req, res) => {
+  // TODO: Add a comment describing the functionality of this if statement
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
 
 module.exports = router;
-
-
